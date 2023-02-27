d2a8c5f65caa40e2c14e3cdc9fbf00dc36e4128e
XxdalendxX <connordr112@gmail.com>
2023-02-27 14:59:38 +1100

    worked on character controller

---
+++ Advanced Physics Assessment/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController controller;
+    GameObject player;
+
+    [Header("Movement")]
+    [SerializeField, Range(5, 25)] float speed = 10;
+    [SerializeField] bool JumpInput;
+    [SerializeField] Vector3 velocity;
+    [SerializeField, Range(-15, -1)] float gravy = -9.8f;
+
+    Vector2 moveInput = new Vector2();
+
+    [Header("Variables")]
+    [SerializeField, Range(5, 25)] float jumpVelocity = 5;
+
+    [Header("Checks")]
+    [SerializeField] bool isGrounded;
+
+    [Header("Ground Check")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+
+
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+
+        speed = 25f;
+
+        velocity.y += gravy * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
